1f626d4cbf380eaf4d58825491caf69628b3366a
a-einollahi <einollahi.ar@gmail.com>
2022-06-12 11:39:53 +0430

    implement stack

---
+++ 1 - Data Structures/02_stack.ts
@@ -0,0 +1,29 @@
+class Stack<T> {
+  private readonly MAX!: number;
+  private top: number = 0;
+  private stack: (T | null)[] = new Array(this.MAX).fill(null);
+
+  constructor(max: number) {
+    this.MAX = max;
+  }
+
+  public isEmpty(): boolean {
+    return this.top < 0;
+  }
+
+  public push(data: T): void {
+    if (this.top >= this.MAX - 1) throw new Error('Stack Overflow');
+    this.stack[this.top++] = data;
+    console.log(this.stack);
+  }
+
+  public pop(): void {
+    if (this.top <= 0) throw new Error('Stack Underflow');
+    this.stack[this.top--] = null;
+  }
+
+  public peek(): T {
+    if (this.top <= 0) throw new Error('Stack Underflow');
+    return this.stack[this.top - 1] as T;
+  }
+}
